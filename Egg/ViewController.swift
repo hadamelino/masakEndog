536a62a4cd9645e75fa46ab3719647e6df9ba02f
Hada <hadamuhammad@gmail.com>
2022-04-05 14:38:14 +0700

    Initial Commit

---
+++ Egg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg
+//
+//  Created by Hada Melino Muhammad on 05/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
