801dbe37adf6973d3d7ba08a4280fea699ecfc55
hadamelino <hadamuhammad@gmail.com>
2022-04-06 00:52:33 +0700

    Adding collection view for banner, add BannerUICollectionViewCell

---
+++ Egg/BannerCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BannerCollectionViewCell.swift
+//  Egg
+//
+//  Created by Hada Melino Muhammad on 05/04/22.
+//
+
+import UIKit
+
+class BannerCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var bannerImage: UIImageView!
+    @IBOutlet weak var bannerText: UILabel!
+    
+}
--- Egg/ViewController.swift
+++ Egg/ViewController.swift
@@ -7,14 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello Masak Endog!")
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
+    
+    var bannerImage = "sample_image"
+    var bannerText = "Eggs of the day"
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "bannerCell", for: indexPath) as! BannerCollectionViewCell
+        cell.bannerImage.image = UIImage(named: bannerImage)
+        cell.bannerImage.layer.cornerRadius = 15
+        cell.bannerText.text = bannerText
+        return cell
+    }
+    
 
 }
 
