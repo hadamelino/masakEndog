fc53cd119acdd278dc3b3c9aaddd7da2ca405c7f
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2022-04-06 17:30:00 +0700

    Add View Controller for Egg Methods Page

---
+++ Egg/EggMethodsController.swift
@@ -0,0 +1,29 @@
+//
+//  EggMethodsController.swift
+//  Egg
+//
+//  Created by Syafitra Alfa Ridzi on 06/04/22.
+//
+
+import UIKit
+
+class EggMethodsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
