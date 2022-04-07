c98e224648e5672f6dc5a69fbac10f27caf73ec3
Zevira Varies Martan <zeviravm@gmail.com>
2022-04-07 17:13:30 +0700

    Add : - detailPage viewController - detialPage storyboard - scrollbar - collection bar & cells - Egg assets

---
+++ Egg/MyCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MyCollectionViewCell.swift
+//  Egg
+//
+//  Created by Zevira varies martan on 07/04/22.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var MyDetailPageImage: UIImageView!
+    
+    
+}
