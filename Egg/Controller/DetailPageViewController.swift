f1a6a62b83fc245c4753f631075c76ce2158ce2b
Zevira Varies Martan <zeviravm@gmail.com>
2022-04-07 22:39:44 +0700

    Add detailPage (scrollview + collection view)

---
+++ Egg/Controller/DetailPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailPageViewController.swift
+//  Egg
+//
+//  Created by Zevira varies martan on 06/04/22.
+//
+
+import UIKit
+
+class DetailPageViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return detailPageImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! DetailPageCollectionViewCell
+        
+        cell.MyDetailPageImage.image = UIImage(named: detailPageImages[indexPath.row])
+        cell.MyDetailPageImage.layer.cornerRadius = 50.0
+        return cell
+        
+        
+    }
+    
+    
+    var detailPageImages: [String] = ["ss1", "ss2", "ss3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
