d8166145890a6a4b85841d25c20e18f22370f874
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2022-04-08 08:48:41 +0700

    Add Navigation Bar Controller

--- Egg/View/EggMethodsController.swift
+++ Egg/View/EggMethodsController.swift
@@ -8,11 +8,15 @@
 import UIKit
 
 class EggMethodsController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.title = "asfaddsfsdfs"
+        
     }
     
 
