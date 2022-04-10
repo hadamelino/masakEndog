d37943e8cd8a7143d8022610e00fcfec3491f738
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2022-04-10 15:16:19 +0700

    Add collection view Add collection view cell Add image assets Fix the grouping

---
+++ Egg/EggMethodsPage/MethodsCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MethodsCollectionViewCell.swift
+//  Egg
+//
+//  Created by Syafitra Alfa Ridzi on 10/04/22.
+//
+
+import UIKit
+
+class MethodsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var methodImageContainer: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
