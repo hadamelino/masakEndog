94e7e086fcd5d3d1915b1528ca6e94e75c02cb44
hadamelino <hadamuhammad@gmail.com>
2022-04-11 16:30:26 +0700

    idk

---
+++ Egg/HomePage/View/RecipeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeCollectionViewCell.swift
+//  Egg
+//
+//  Created by Hada Melino Muhammad on 11/04/22.
+//
+
+import UIKit
+
+class RecipeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var recipeName: UILabel!
+}
