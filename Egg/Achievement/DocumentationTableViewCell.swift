fc67fcf8a78c18b80ca2348dffbea1b30686834b
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2022-04-14 00:52:42 +0700

    Achievement Page (Semi-Completed) Add DocumentationViewCell and xib file

---
+++ Egg/Achievement/DocumentationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DocumentationTableViewCell.swift
+//  Egg
+//
+//  Created by Syafitra Alfa Ridzi on 14/04/22.
+//
+
+import UIKit
+
+class DocumentationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
