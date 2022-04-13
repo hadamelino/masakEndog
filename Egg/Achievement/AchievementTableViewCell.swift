6bdfa6513591287325002926b965b0830728b139
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2022-04-13 22:56:36 +0700

    Add Table View (Work)

---
+++ Egg/Achievement/AchievementTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AchievementTableViewCell.swift
+//  Egg
+//
+//  Created by Syafitra Alfa Ridzi on 13/04/22.
+//
+
+import UIKit
+
+class AchievementTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var achievementCellView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Egg/Achievement/AchievementViewController.swift
+++ Egg/Achievement/AchievementViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class AchievementViewController: UIViewController {
 
+    @IBOutlet weak var awardsSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var awardsTableView: UITableView!
+    
+    var colorArr:[UIColor]  = [.red, .gray, .cyan, .brown, .green, .blue, .black]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        awardsTableView.register(UINib(nibName: "AchievementTableViewCell", bundle: nil), forCellReuseIdentifier: "AchievementTableViewCell")
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension AchievementViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colorArr.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = awardsTableView.dequeueReusableCell(withIdentifier: "AchievementTableViewCell", for: indexPath) as! AchievementTableViewCell
+        cell.achievementCellView.backgroundColor = colorArr[indexPath.row]
+        return cell
+    }
+    
+    
 }
