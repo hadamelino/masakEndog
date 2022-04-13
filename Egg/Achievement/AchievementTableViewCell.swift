815b17acbba216c02e6829a402cdbf44af667d20
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2022-04-14 00:40:41 +0700

    Add Achievement Assets Add Achievement Data Struct Add Data to Cell View

--- Egg/Achievement/AchievementTableViewCell.swift
+++ Egg/Achievement/AchievementTableViewCell.swift
@@ -10,6 +10,19 @@ import UIKit
 class AchievementTableViewCell: UITableViewCell {
 
     @IBOutlet weak var achievementCellView: UIView!
+    @IBOutlet weak var bannerImage: UIImageView!
+    @IBOutlet weak var bannerLabel: UILabel!
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var secondImage: UIImageView!
+    @IBOutlet weak var thirdImage: UIImageView!
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
+    @IBOutlet weak var firstDetailLabel: UILabel!
+    @IBOutlet weak var secondDetailLabel: UILabel!
+    @IBOutlet weak var thirdDetailLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Egg/Achievement/AchievementViewController.swift
+++ Egg/Achievement/AchievementViewController.swift
@@ -13,9 +13,11 @@ class AchievementViewController: UIViewController {
     @IBOutlet weak var awardsTableView: UITableView!
     
     var colorArr:[UIColor]  = [.red, .gray, .cyan, .brown, .green, .blue, .black]
+    var achievementData: AchievementData?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        achievementData = getAchievementDetail()
         awardsTableView.register(UINib(nibName: "AchievementTableViewCell", bundle: nil), forCellReuseIdentifier: "AchievementTableViewCell")
         // Do any additional setup after loading the view.
     }
@@ -23,14 +25,22 @@ class AchievementViewController: UIViewController {
 
 extension AchievementViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return colorArr.count
+        return achievementData!.bannerLabel.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = awardsTableView.dequeueReusableCell(withIdentifier: "AchievementTableViewCell", for: indexPath) as! AchievementTableViewCell
-        cell.achievementCellView.backgroundColor = colorArr[indexPath.row]
+        cell.bannerImage.image      = achievementData?.bannerImage[indexPath.row]
+        cell.bannerLabel.text       = achievementData?.bannerLabel[indexPath.row]
+        cell.firstImage.image       = achievementData?.firstImage[indexPath.row]
+        cell.secondImage.image      = achievementData?.secondImage[indexPath.row]
+        cell.thirdImage.image       = achievementData?.thirdImage[indexPath.row]
+        cell.firstLabel.text        = achievementData?.firstLabel[indexPath.row]
+        cell.secondLabel.text       = achievementData?.secondLabel[indexPath.row]
+        cell.thirdLabel.text        = achievementData?.thirdLabel[indexPath.row]
+        cell.firstDetailLabel.text  = achievementData?.firstDetailLabel[indexPath.row]
+        cell.secondDetailLabel.text = achievementData?.secondDetailLabel[indexPath.row]
+        cell.thirdDetailLabel.text  = achievementData?.thirdDetailLabel[indexPath.row]
         return cell
     }
-    
-    
 }
