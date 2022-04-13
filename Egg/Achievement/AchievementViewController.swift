f12f4a37de7a45501ecd66270a950a872d2fab02
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2022-04-14 01:57:20 +0700

    Move assets to achievements folder Add Documentation Page Achievement and Documentation Page ( Semi Finish ) -> View

--- Egg/Achievement/AchievementViewController.swift
+++ Egg/Achievement/AchievementViewController.swift
@@ -14,33 +14,61 @@ class AchievementViewController: UIViewController {
     
     var colorArr:[UIColor]  = [.red, .gray, .cyan, .brown, .green, .blue, .black]
     var achievementData: AchievementData?
+    var documentationData: DocumentationData?
+    
+    @IBAction func segmentedViewControlChange(_ sender: Any) {
+        if awardsSegmentedControl.selectedSegmentIndex == 1{
+            awardsTableView.rowHeight = 275
+        }
+        awardsTableView.reloadData()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        achievementData = getAchievementDetail()
+        achievementData     = getAchievementDetail()
+        documentationData   = getDocumentationData()
         awardsTableView.register(UINib(nibName: "AchievementTableViewCell", bundle: nil), forCellReuseIdentifier: "AchievementTableViewCell")
-        // Do any additional setup after loading the view.
+        awardsTableView.register(UINib(nibName: "DocumentationTableViewCell", bundle: nil), forCellReuseIdentifier: "DocumentationTableViewCell")
     }
 }
 
 extension AchievementViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return achievementData!.bannerLabel.count
+        if awardsSegmentedControl.selectedSegmentIndex == 0 {
+            return achievementData!.bannerLabel.count
+        }else {
+            return documentationData!.firstTitleLabel.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = awardsTableView.dequeueReusableCell(withIdentifier: "AchievementTableViewCell", for: indexPath) as! AchievementTableViewCell
-        cell.bannerImage.image      = achievementData?.bannerImage[indexPath.row]
-        cell.bannerLabel.text       = achievementData?.bannerLabel[indexPath.row]
-        cell.firstImage.image       = achievementData?.firstImage[indexPath.row]
-        cell.secondImage.image      = achievementData?.secondImage[indexPath.row]
-        cell.thirdImage.image       = achievementData?.thirdImage[indexPath.row]
-        cell.firstLabel.text        = achievementData?.firstLabel[indexPath.row]
-        cell.secondLabel.text       = achievementData?.secondLabel[indexPath.row]
-        cell.thirdLabel.text        = achievementData?.thirdLabel[indexPath.row]
-        cell.firstDetailLabel.text  = achievementData?.firstDetailLabel[indexPath.row]
-        cell.secondDetailLabel.text = achievementData?.secondDetailLabel[indexPath.row]
-        cell.thirdDetailLabel.text  = achievementData?.thirdDetailLabel[indexPath.row]
-        return cell
+        if awardsSegmentedControl.selectedSegmentIndex == 0 {
+            let cell = awardsTableView.dequeueReusableCell(withIdentifier: "AchievementTableViewCell", for: indexPath) as! AchievementTableViewCell
+            cell.bannerImage.image      = achievementData?.bannerImage[indexPath.row]
+            cell.bannerLabel.text       = achievementData?.bannerLabel[indexPath.row]
+            cell.firstImage.image       = achievementData?.firstImage[indexPath.row]
+            cell.secondImage.image      = achievementData?.secondImage[indexPath.row]
+            cell.thirdImage.image       = achievementData?.thirdImage[indexPath.row]
+            cell.firstLabel.text        = achievementData?.firstLabel[indexPath.row]
+            cell.secondLabel.text       = achievementData?.secondLabel[indexPath.row]
+            cell.thirdLabel.text        = achievementData?.thirdLabel[indexPath.row]
+            cell.firstDetailLabel.text  = achievementData?.firstDetailLabel[indexPath.row]
+            cell.secondDetailLabel.text = achievementData?.secondDetailLabel[indexPath.row]
+            cell.thirdDetailLabel.text  = achievementData?.thirdDetailLabel[indexPath.row]
+            cell.selectionStyle         = .none
+            return cell
+        }else {
+            let cell = awardsTableView.dequeueReusableCell(withIdentifier: "DocumentationTableViewCell", for: indexPath) as! DocumentationTableViewCell
+            cell.firstTitleLabel.text     = documentationData?.firstTitleLabel[indexPath.row]
+            cell.secondTitleLabel.text    = documentationData?.secondTitleLabel[indexPath.row]
+            cell.firstImage.image         = documentationData?.firstImage[indexPath.row]
+            cell.secondImage.image        = documentationData?.secondImage[indexPath.row]
+            cell.firstDatetimeLabel.text  = documentationData?.firstDatetimeLabel[indexPath.row]
+            cell.secondDatetimeLabel.text = documentationData?.secondDatetimeLabel[indexPath.row]
+            cell.firstImage.layer.cornerRadius  = 15
+            cell.secondImage.layer.cornerRadius = 15
+            cell.selectionStyle = .none
+            return cell
+        }
     }
 }
--- Egg/Achievement/DocumentationTableViewCell.swift
+++ Egg/Achievement/DocumentationTableViewCell.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class DocumentationTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var secondImage: UIImageView!
+    @IBOutlet weak var firstTitleLabel: UILabel!
+    @IBOutlet weak var secondTitleLabel: UILabel!
+    @IBOutlet weak var firstDatetimeLabel: UILabel!
+    @IBOutlet weak var secondDatetimeLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
